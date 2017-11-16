a597b41777752847d20049c6a7734e2f12e08f1c
denis-koptev <koptevda@ya.ru>
2017-11-15 17:10:32 -0800

    More Jenkins tests added

--- test_site/test_site/settings.py
+++ test_site/test_site/settings.py
@@ -41,11 +41,11 @@ INSTALLED_APPS = [
     'django_jenkins',
 ]
 
-JENKINS_TASKS = (
-	'django_jenkins.tasks.run_pylint',
-	'django_jenkins.tasks.run_pep8',
-	'django_jenkins.tasks.run_pyflakes',
-)
+JENKINS_TASKS = ('django_jenkins.tasks.run_pylint',
+                 'django_jenkins.tasks.run_pep8',
+                 'django_jenkins.tasks.run_pyflakes',
+                 'django_jenkins.tasks.with_coverage',
+                 'django_jenkins.tasks.django_tests',)
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
