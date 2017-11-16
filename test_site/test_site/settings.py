89dbba6a0f8f60ad90a88f22ea191043f3560a5f
Denis Koptev <koptevda@ya.ru>
2017-11-16 05:35:48 +0300

    Update settings.py

--- test_site/test_site/settings.py
+++ test_site/test_site/settings.py
@@ -43,9 +43,7 @@ INSTALLED_APPS = [
 
 JENKINS_TASKS = ('django_jenkins.tasks.run_pylint',
                  'django_jenkins.tasks.run_pep8',
-                 'django_jenkins.tasks.run_pyflakes',
-                 'django_jenkins.tasks.with_coverage',
-                 'django_jenkins.tasks.django_tests',)
+                 'django_jenkins.tasks.run_pyflakes',)
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
